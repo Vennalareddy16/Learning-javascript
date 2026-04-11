1e4c0ea4d7b5aaadaab64ff1996cb16e2fabef21
Vennalareddy16 <vennalareddy1626@gmail.com>
2026-04-11 09:27:12 +0530

    added file

---
+++ .vscode/day2/script.js
@@ -0,0 +1,25 @@
+//datatypes
+
+/*var a=10;
+var a=20;
+console.log(a);
+
+
+let c=32;
+c=40;
+console.log(c);
+
+const b=56;
+console.log(b);*/
+
+
+//Arrays
+
+let userData=[10, "hello", 5.64, false, null]
+
+for(let index=0;index<userData.length;index++)
+{
+    console.log(userData[index]);
+    console.log(typeof userData[index]);
+}
+userData.push("java");
---
+++ script.js
@@ -0,0 +1,48 @@
+console.log(-1);
+
+var num1=100000;
+var num2 =100;
+
+if(num1 < num2){
+    console.log(-1);
+    
+}else{
+    console.log(1);
+    
+}
+
+var a=10;
+var b="10";
+if(a === b) console.log(-1);
+
+console.log(2 ** 4);
+
+var c=10;
+
+for(var i=1 ; i<=c ; i++){
+    var fc=0;
+    for(var j=1 ; j<=i;j++){
+        
+
+        if(i%j==0){
+            fc++;
+        }
+    }
+    if(fc == 2){
+        console.log(i);
+    }
+}
+
+
+var a=10;
+var a=20;
+console.log(a);
+
+
+let c=32;
+c=40;
+console.log(c);
+
+const b=56;
+console.log(b);
+
